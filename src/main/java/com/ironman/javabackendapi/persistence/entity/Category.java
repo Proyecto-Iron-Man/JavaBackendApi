5522d71e7ea021cf43396f14e26694afd2d25f48
AbelPalominoRojas <abel.palominorojas@gmail.com>
2024-04-17 21:58:06 -0500

    feat: add Category entity

---
+++ src/main/java/com/ironman/javabackendapi/persistence/entity/Category.java
@@ -0,0 +1,34 @@
+package com.ironman.javabackendapi.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+// Lombok anotations
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+// JPA anotations
+@Entity
+// @Table(name = "categories")
+public class Category implements Serializable {
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    private String name;
+    private String description;
+    private String state;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
