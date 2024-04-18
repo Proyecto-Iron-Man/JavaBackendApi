d6103193eb7174c811114dd8a352aebcbd14c271
AbelPalominoRojas <abel.palominorojas@gmail.com>
2024-04-17 21:09:00 -0500

    Start Project

---
+++ src/main/java/com/ironman/javabackendapi/JavaBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.ironman.javabackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironman/javabackendapi/JavaBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironman.javabackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
